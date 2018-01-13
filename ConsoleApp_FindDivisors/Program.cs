4abf058a8473b12a3669f2ef6f024b1442e66013
vsk <m-vsk@yandex.ru>
2018-01-13 22:55:44 +0300

    Find the divisors!

--- ConsoleApp_FindDivisors/Program.cs
+++ ConsoleApp_FindDivisors/Program.cs
@@ -6,6 +6,16 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp_FindDivisors
 {
+    public class Kata
+    {
+        public static int[] Divisors(int n)
+        {
+            int[] div;
+
+            return div;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
