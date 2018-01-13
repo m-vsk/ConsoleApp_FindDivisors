a3aecca989ea32d7479b151b629f873b5cae2989
vsk <m-vsk@yandex.ru>
2018-01-14 00:23:54 +0300

    update

--- ConsoleApp_FindDivisors/Program.cs
+++ ConsoleApp_FindDivisors/Program.cs
@@ -10,16 +10,32 @@ namespace ConsoleApp_FindDivisors
     {
         public static int[] Divisors(int n)
         {
-            int[] div;
-
-            return div;
+            var div = from a in Enumerable.Range(2, n / 2) where n % a == 0 select a;
+            if (div.Count() > 0)
+                return div.ToArray();
+            else return new int[] {};
         }
     }
+
+/*
+            Assert.AreEqual(new int[] { 3, 5 }, Kata.Divisors(15));
+            Assert.AreEqual(new int[] { 2, 4, 8 }, Kata.Divisors(16));
+            Assert.AreEqual(new int[] { 11, 23 }, Kata.Divisors(253));
+            Assert.AreEqual(new int[] { 2, 3, 4, 6, 8, 12 }, Kata.Divisors(24));
+*/
 
     class Program
     {
         static void Main(string[] args)
         {
+
+            foreach (var i in Kata.Divisors(13))
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.ReadLine();
+
         }
     }
 }
---
+++ ConsoleApp_FindDivisorsTests/KataTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp_FindDivisors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_FindDivisors.Tests
+{
+    [TestClass()]
+    public class KataTests
+    {
+        [TestMethod()]
+        public void DivisorsTest()
+        {
+            Assert.AreEqual(new int[] { 3, 5 }, Kata.Divisors(15));
+        }
+    }
+}
